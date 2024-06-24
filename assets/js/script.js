9a6ef709b7d379d0f326f3ff1ea678df2c2a904c
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-06-24 16:09:04 +0000

    Add code for the fluids count

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,28 +5,26 @@
 function calculateBMI() {
   let heightInput = document.getElementById("height");
   let weightInput = document.getElementById("weight");
-  let result = document.getElementById("result");
+  let resultDiv = document.getElementById("result");
 
   var height = parseFloat(heightInput.value);
   var weight = parseFloat(weightInput.value);
 
-  if (weight <= "1" && weight === "500") {
-    alert("Please enter your weight.");
-    return;
+  if (weight <= 1 && weight === 500) {
+    return ["Please enter your weight."];
   }
 
   if (height <= 1 && height === 350) {
-    alert("Please enter your height.");
-    return;
+    return ["Please enter your height."];
   }
 
   var bmi = weight / ((height / 100) ** 2);
   var classification = "";
 
-/**
- * Tell the result to the user and
- * tell the user in their BMI
- */
+  /**
+   * Tell the result to the user and
+   * tell the user in their BMI
+   */
   if (bmi > 1 && bmi <= 16) {
     classification = "Severly underweight"
   } else if (bmi > 16 && bmi <= 18.5) {
@@ -43,36 +41,35 @@ function calculateBMI() {
     alert("Enter valid weight and height")
   }
 
-  result.innerHTML = "This is your BMI " + bmi.toFixed(2) + " (" + classification + ")";
+  resultDiv.innerHTML = "This is your BMI: " + bmi.toFixed(2) + " (" + classification + ")";
 }
 
 let popup = document.getElementById("popup");
 
-  function openPopup(){
-    popup.classList.add("open-popup").innerHTML;
-  }
+function openPopup() {
+  popup.classList.add("open-popup").innerHTML;
+}
 
-  function closePopup(){
-    popup.classList.remove("open-popup").innerHTML;
-  }
+function closePopup() {
+  popup.classList.remove("open-popup").innerHTML;
+}
 
 /**
  * Function to add the amount of fluids drank 
  * and return the totla amount inputed 
  */
- document.addEventListener("DOMContent-Loaded", function(event) =>
-  { let totalFluids = 0; 
-  function addFluids() { 
-    let fluidsInput = document.getElementById('fluids').value; 
-    let fluidsAmount = parseInt(fluidsInput); 
+const inp1 = document.getElementById("fluids");
+const tot1 = document.getElementById("total-amount");
+const btn1 = document.getElementById("add-fluids-btn");
 
-    if (!isNaN(fluidsAmount) && fluidsAmount > 0) { 
-      totalFluids += fluidsAmount; 
-      document.getElementById('total-amount').textContent = totalFluids; 
-      document.getElementById('fluids').value = ''; 
-    } else { alert('Please enter a valid number');    
-    } 
-    }
-    let addFluidsBtn = document.getElementById("add-fluids-btn");
-    addFluidsBtn.addEventListener("onclick", addFluids);
-  });
+function addFluids() {
+  tot1.innerHTML = (inp1 + btn1).value;
+
+  if (tot1 >= 1500){
+    return["Congradulations! You have reached your daily target"]
+  } else {
+    return ["Continue drinking. Your are on the right track"]
+  }
+}
+btn1.addEventListener("click", addFluids );
+
